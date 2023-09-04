52513a8c14518af6f096c941e0337bb83074b113
mister stam <stam@stam.com>
2023-09-04 22:32:02 +0300

    make vars for each info item, set the element text from it, use it to open various links. also remove the min width used before to fit the linkedin link. now it is shorter the issue with the homepage link error is not solved yet. seems like a certificate issue

--- scripts/events.js
+++ scripts/events.js
@@ -1,4 +1,10 @@
+var link_phone = "+972 52 420 5851";
+var link_email = "ehud8vardi@gmail.com";
 var link_homepage = "https://www.ehudvardi.github.io";
+var link_linkedin = "https://www.linkedin.com/in/ehudvardi";
+var link_location = "Ra'anana, IL";
+
+
 
 // Wait for the page to fully load before attaching the event listeners
 document.addEventListener("DOMContentLoaded", function() {
@@ -10,29 +16,33 @@ document.addEventListener("DOMContentLoaded", function() {
 		elementC = elements[i];
 		switch (elementC.id) {
 		case "phone":
+			elementC.textContent = link_phone;
 			elementC.addEventListener("click", function() {
-				alert("phone");
+				// alert("phone");
 			});
 			break; 
 		case "email":
+			elementC.textContent = link_email;
 			elementC.addEventListener("click", function() {
-				alert("email");
+				window.location.href = "mailto:" + link_email;
 			});
 			break; 
 		case "homepage":
-			elementC.textContent = link_homepage;
+			elementC.textContent = link_homepage.replace("https://www.","");
 			elementC.addEventListener("click", function() {
 				window.open(link_homepage, "_blank");
 			});
 			break; 
 		case "linkedin":
+			elementC.textContent = link_linkedin.replace("https://www.","");
 			elementC.addEventListener("click", function() {
-				alert("linkedin");
+				window.open(link_linkedin, "_blank");
 			});
 			break; 
 		case "location":
+			elementC.textContent = link_location;
 			elementC.addEventListener("click", function() {
-				alert("location");
+				window.open("http://maps.google.com/?q=" + link_location);
 			});
 			break; 
 		default:
